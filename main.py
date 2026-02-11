8b748cd84e3046e87440ae7b191af2c7dfd23750
madroots <simon.barker@linuxmail.org>
2026-02-11 19:57:22 +0100

    Feat: Bundle cloudflared binaries in AppImage and EXE for zero-dependency portability

--- main.py
+++ main.py
@@ -4,7 +4,23 @@ from tunnelflare_gui.main_window import MainWindow
 from tunnelflare_gui.styles import Styles
 import signal
 
+def setup_bundled_path():
+    import os
+    import sys
+    
+    # Handle PyInstaller (Windows)
+    if hasattr(sys, '_MEIPASS'):
+        os.environ['PATH'] = sys._MEIPASS + os.pathsep + os.environ['PATH']
+    
+    # Handle AppImage (Linux)
+    # The AppRun script usually handles this, but let's be safe.
+    appdir = os.environ.get('APPDIR')
+    if appdir:
+        bundled_bin = os.path.join(appdir, 'usr', 'bin')
+        os.environ['PATH'] = bundled_bin + os.pathsep + os.environ['PATH']
+
 def main():
+    setup_bundled_path()
     app = QApplication(sys.argv)
     app.setStyleSheet(Styles.STYLESHEET)
     # Allow headers/fonts to look better on high DPI
